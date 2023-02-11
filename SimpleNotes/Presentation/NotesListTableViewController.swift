59dafc369be42fbce6ec7a4d13590e920811e7ce
JuliaRomanenko <rja-97@yandex.ru>
2023-02-11 19:35:57 +0300

    Добавила UITableViewController и настроила его отображние

---
+++ SimpleNotes/Presentation/NotesListTableViewController.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+private let cellIdentifier = "noteCell"
+
+class NotesListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+        configNavigationBar()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        return cell
+    }
+}
+
+extension NotesListTableViewController {
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: cellIdentifier
+        )
+    }
+    
+    private func configNavigationBar() {
+        title = "Ваши заметки"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewNote))
+        navigationController?.navigationBar.tintColor = .red
+    }
+    
+    @objc func addNewNote() {
+        print("Button tap")
+    }
+}
