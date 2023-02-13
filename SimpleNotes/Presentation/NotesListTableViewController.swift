30d0cfd3cbebb0b09e8caf3c61d8930ce9d32e6f
JuliaRomanenko <rja-97@yandex.ru>
2023-02-13 14:32:48 +0300

    Добавила получение массива notes

--- SimpleNotes/Presentation/NotesListTableViewController.swift
+++ SimpleNotes/Presentation/NotesListTableViewController.swift
@@ -4,24 +4,43 @@ private let cellIdentifier = "noteCell"
 
 class NotesListTableViewController: UITableViewController {
     
+    private var notes: [Note] = []
+    private var storage = StorageManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
         configNavigationBar()
+        fetchNotes()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        notes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        let note = notes[indexPath.row]
+        
+        cell.textLabel?.text = note.title
         
         return cell
     }
 }
 
 extension NotesListTableViewController {
+    private func fetchNotes() {
+        storage.fetchNotes { result in
+            switch result {
+            case .success(let notesList):
+                self.notes = notesList
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
