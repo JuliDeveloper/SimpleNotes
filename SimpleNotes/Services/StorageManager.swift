3dac83da7f926b0485b586184914bc7500ed40a8
JuliaRomanenko <rja-97@yandex.ru>
2023-02-13 14:07:31 +0300

    Добавила entity и вынесла работу с CoreData в отдельный класс StorageManager

---
+++ SimpleNotes/Services/StorageManager.swift
@@ -0,0 +1,64 @@
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "SimpleNotes")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    private let viewContext: NSManagedObjectContext
+    
+    private init() {
+        viewContext = persistentContainer.viewContext
+    }
+    
+    // MARK: - Metods
+    func fetchNotes(_ completion: (Result<[Note], Error>) -> Void) {
+        let request = Note.fetchRequest()
+        
+        do {
+            let notes = try viewContext.fetch(request)
+            completion(.success(notes))
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    func save(note: Note, _ completion: (Note) -> Void) {
+        let note = Note(context: viewContext)
+        completion(note)
+        saveContext()
+    }
+    
+    func edit(note: Note, newTitle: String, newBody: String) {
+        note.title = newTitle
+        note.body = newBody
+        saveContext()
+    }
+    
+    func delete(note: Note) {
+        viewContext.delete(note)
+        saveContext()
+    }
+    
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
